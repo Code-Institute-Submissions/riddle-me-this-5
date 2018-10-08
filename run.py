e446d0807ed478e3451a3535136aaa73fb0a4564
Eddie Jagger <eddiejcranmer@gmail.com>
2018-10-08 21:14:29 +0000

    TESTING - Passing data from a View to a Template

--- run.py
+++ run.py
@@ -14,27 +14,27 @@ def index():
     
 @app.route('/africa_quiz')
 def africa():
-    return render_template("africa_quiz.html")
+    return render_template("africa_quiz.html", region="Africa")
     
 @app.route('/asia_quiz')
 def asia():
-    return render_template("asia_quiz.html")
+    return render_template("asia_quiz.html", region="Asia")
     
 @app.route('/australia_quiz')
 def australia():
-    return render_template("australia_quiz.html")
+    return render_template("australia_quiz.html", region="Australia")
     
 @app.route('/europe_quiz')
 def europe():
-    return render_template("europe_quiz.html")
+    return render_template("europe_quiz.html", region="Europe")
     
 @app.route('/n_america_quiz')
 def n_america():
-    return render_template("n_america_quiz.html")
+    return render_template("n_america_quiz.html", region="North America")
 
 @app.route('/s_america_quiz')
 def s_america():
-    return render_template("s_america_quiz.html")
+    return render_template("s_america_quiz.html", region="South America")
 
 
 if __name__ == '__main__':
