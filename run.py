c4bdf2d4e59fbc45d25df2660c9e697f669dd78d
Eddie Jagger <eddiejcranmer@gmail.com>
2018-10-08 20:14:14 +0000

    TESTING - Routing and Block Content/Logic Tags

--- run.py
+++ run.py
@@ -11,9 +11,20 @@ def index():
     with open("data/quiz.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("index.html", quiz_data=data)
+    
+@app.route('/africa_quiz')
+def africa():
+    return render_template("africa_quiz.html")
+    
+@app.route('/asia_quiz')
+def asia():
+    return render_template("asia_quiz.html")
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+            
+def quiz():
+    print("where will this print to??")
